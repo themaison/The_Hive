e0c2d8ac939829f31783ae5cbae5550b858f0a20
Vladislav Melnichuk <melnichuk1712@mail.ru>
2023-05-07 23:00:13 +0300

    Beginning of the FlowerSpawner Script

--- Assets/Scripts/FlowerSpawner.cs
+++ Assets/Scripts/FlowerSpawner.cs
@@ -8,20 +8,51 @@ public class FlowerSpawner : MonoBehaviour
     private GameObject violetObj;
     [SerializeField]
     private GameObject sunflowerObj;
+
     [SerializeField]
     [Range(1, 10)]
     private float forbiddenRadius;
     [SerializeField]
     [Range(2, 15)]
     private float maxRadius;
+    [SerializeField]
+    [Range(1, 10)]
+    private float spawnDelay;
+    private float nextSpawnTime;
+    [SerializeField]
+    [Range(1, 20)]
+    private int spawnLimit;
+    private int spawnCounter;
+
     void Start()
     {
-        
+        spawnCounter = 0;
+        nextSpawnTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Проверка, достигнут ли лимит объектов
+        if (spawnCounter >= spawnLimit)
+            return;
+
+        // Проверка, прошла ли нужная задержка между спавнами
+        else if (Time.time >= nextSpawnTime)
+        {
+            // Спавн объекта и обновление времени следующего спавна
+            Vector2 spawnPosition = GetRandSpawnPosition();
+            Instantiate(sunflowerObj, spawnPosition, Quaternion.identity);
+            nextSpawnTime = Time.time + spawnDelay;
+            spawnCounter++;
+        }
+    }
+
+    private Vector2 GetRandSpawnPosition()
+    {
+        float randX = Random.Range(-10, 10);
+        float randY = Random.Range(-10, 10);
+        Vector2 spawnPosition = new Vector2(randX, randY);
+        return spawnPosition;
     }
 }
